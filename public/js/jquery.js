4ce25899382692664cceb78d1a66a0db6244e3b8
Joris Veldt <Joris@MacBook-Pro-8.local>
2018-10-05 16:48:41 +0200

    add own post page and footer

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -5,7 +5,3 @@ $("#relocate_login").on("click", function relocation_signin() {
 $("#relocate_signup").on("click", function relocation_signup() {
 	location.href = "/signup";
 })
-
-$("#password").on("click", function password() {
-  $( "#show-password" ).show( "slow" );
-});
